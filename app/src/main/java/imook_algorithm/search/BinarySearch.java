a5f5c5a1ab9d7cf48d6688f08899f32d5624ce01
xzc <xingzhicheng@imudges.com>
2024-10-14 02:32:55 +0800

    二分查找法变种 1.upper(求大于target的最小索引) 2.ceil(求不小于target的最大索引)

--- app/src/main/java/imook_algorithm/search/BinarySearch.java
+++ app/src/main/java/imook_algorithm/search/BinarySearch.java
@@ -44,4 +44,41 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    //>target的最小索引值
+    public static <E extends Comparable<E>> int upper(E[] data, E target){
+        int l=0,r= data.length;
+        //在data[l,r]中寻找解
+        while (l<r){
+            int mid =l+(r-l)/2;
+            if(data[mid].compareTo(target)<0)
+                l=mid+1;
+            else
+                r=mid;
+        }
+        return l;
+    }
+
+    //> target,返回大于target的最小值索引
+    //== target,返回target的最大索引
+    public static <E  extends Comparable<E>>  int  ceil(E[] data,E target){
+        int u=upper(data,target);
+        if(data[u-1].compareTo(target)==0)
+            return u-1;
+        return u;
+    }
+ public static void main(String[] args) {
+
+        Integer[] arr={1,2,3,3,5,5};
+
+     for (int i = 0; i <=6 ; i++)
+//         System.out.print(BinarySearch.upper(arr,i)+" ");
+
+         System.out.print(BinarySearch.upper(arr,i)+" ");
+
+     System.out.println();
+
+
+
+    }
 }
